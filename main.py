6544916f81aeac6a9d014089dc598d7dbe5aad6e
Nguyen Ngoc Han <hannguyenngoc0506@gmail.com>
2026-06-09 14:58:59 +0700

    xin chao minh la nh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def tinh_thue_thu_nhap(thu_nhap):
   # Dev B áp dụng thuế 10%
   return 0.1
-  print ("xin chao minh la ngoc han day")
+  print ("xin chao minh la ngoc han day",thu_nhap)
   #222222222222
